5ab2414f1c29b5e80fb8039968e0d9b7edd21763
Sharmin Akter <72566503+SharminAkter1@users.noreply.github.com>
2022-01-27 05:30:16 +0600

    show alert message

--- ui.js
+++ ui.js
@@ -10,26 +10,65 @@ class UI {
         <div class = "row">
           <div class = "col-md-3">
              <img class="img-fluid mb-2" src = "${user.avatar_url}">
-             <a href = "${user.html_url}" target= "_blank" class="btn btn-primary btn-block mb-4"> View Profile</a>
+             <a href = "${user.html_url}" target= "_blank" class="btn btn-primary btn-block mb-4 ">View Profile</a>
           </div>
-          <div class = "col-mb-9">
-           <span class = "badge badge-primary">Public Repos: ${user.public_repos}</span>
-           <span class = "badge badge-secondary">Public Gists: ${user.public_gists}</span>
-           <span class = "badge badge-success">Followers: ${user.followers}</span>
-           <span class = "badge badge-info">Following: ${user.public_following}</span>
+          <div class ="col-md-9">
+          <span class="btn btn-primary mb-2">Public Repos: ${user.public_repos}</span>
+          <span class="btn btn-secondary mb-2">Public Gists: ${user.public_gists}</span>
+          <span class="btn btn-success mb-2">Followers: ${user.followers}</span>
+          <span class="btn btn-info">Following: ${user.following}</span>
+         
            <br><br>
            <ul class="list-group">
             <li class="list-group-item">Company: ${user.company}</li>
-            <li list-group-item">Blog: ${user.blog}</li>
-            <li list-group-item">Location: ${user.location}</li>
-            <li list-group-item">Member Since: ${user.created_at}</li>
+            <li class="list-group-item">Blog: ${user.blog}</li>
+            <li class="list-group-item">Location: ${user.location}</li>
+            <li class="list-group-item">Member Since: ${user.created_at}</li>
            </ul>
           </div>
         </div>
       </div>
       <h3 class="page-heading mb-3">Latest Repos</h3>
-      <div id=""></div>
+      <div id="repos"></div>
     `;
 
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add Text
+    div.appendChild(document.createTextNode(message));
+    // Get Parent
+    const container = document.querySelector('.searchContainer');
+    // Get Search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Time out after 3 second
+    setTimeout(() => {
+       this.clearAlert();
+    }, 3000);
+
+  }
+
+  // Clear Alert
+  clearAlert(){
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+  
+  // Clear Profile
+  clearProfile() {
+    this.profile.innerHTML = '';
   }
 }
